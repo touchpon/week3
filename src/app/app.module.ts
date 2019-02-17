13e81709667ef5f2aae35805975816e9e8ee007b
touchpon <touchpon20838@gmail.com>
2019-02-17 10:56:18 +0700

    test commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {FormsModule,ReactiveFormsModule} from '@angular/forms';
+import { DetailComponent } from './detail/detail.component';
+import { FormComponent } from './form/form.component';
+imports:[BrowserModule,FormsModule,ReactiveFormsModule];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DetailComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
